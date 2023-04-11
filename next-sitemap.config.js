179402c516f84694e8656fafe3b69fcb2c72a559
Ramon Gil <ramon.gil@deporvillage.com>
2023-04-11 10:14:34 +0200

    feat(jornades-pad): [with-sitemap] Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.jornadespad.cat',
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    generateRobotsTxt: true,
+};
