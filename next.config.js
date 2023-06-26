a3b10a7a8ee897f5405bc46e647981fe8f1e14c9
Ramon Gil <ramon.gil@deporvillage.com>
2023-06-26 17:55:31 +0200

    chore(jornades-pad): [with-latest-next] Added nextjs latest version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        formats: ['image/avif', 'image/webp'],
+    },
+};
 
 module.exports = nextConfig
